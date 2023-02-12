ad35c4aeb4f233c4e34303681a1485f8182021e7
Nikolay Titenin <16091969@bk.ru>
2023-02-12 19:22:02 +0300

    code optimization

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 2;
         int num2 = 5;
-        char sign = '+';
+        char sign = '^';
         int result = num1 + num2;
         if (sign == '-') {
             result = num1 - num2;
@@ -12,14 +12,9 @@ public class Calculator {
         } else if (sign == '/') {
             result = num1 / num2;
         } else if (sign == '^') {
-            if (num2 > 1) {
-                int multiplier = num1;
-                for (int i = 2; i <= num2; i++) {
-                    result = multiplier * num1;
-                    multiplier *= num1;
-                }
-            } else {
-                result = num1;
+            result = 1;
+            for (int i = 1; i <= num2; i++) {
+                result *= num1;
             }
         } else if (sign == '%') {
             result = num1 % num2;
--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -3,30 +3,16 @@ public class MyFirstGame {
     public static void main(String[] args) {
         int hiddenNumber = 52;
         int playerNumber = 100 / 2;
-        if (playerNumber > hiddenNumber) {
-            System.out.println("Число игрока больше того, что загадал компьютер.");
-            for (int i = 0; i < 50; i++) {
+        while (playerNumber != hiddenNumber) {
+            if (playerNumber > hiddenNumber) {
+                System.out.println("Число игрока больше того, что загадал компьютер.");
                 playerNumber--;
-                if (playerNumber > hiddenNumber) {
-                    System.out.println("Число игрока больше того, что загадал компьютер.");
-                } else {
-                    System.out.println("Вы победили!");
-                    break;
-                }
-            }
-        } else if (playerNumber < hiddenNumber) {
-            System.out.println("Число игрока меньше того, что загадал компьютер.");
-            for (int j = 0; j < 50; j++) {
+            } else {
+                System.out.println("Число игрока меньше того, что загадал компьютер.");
                 playerNumber++;
-                if (playerNumber < hiddenNumber) {
-                    System.out.println("Число игрока меньше того, что загадал компьютер.");
-                } else {
-                    System.out.println("Вы победили!");
-                    break;
-                }
             }
-        } else {
-            System.out.println("Вы победили!");
         }
+
+        System.out.println("Вы победили!");
     }
 }
